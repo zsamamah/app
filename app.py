dc388e7823d12803233e66296fd7977327da10e7
zsamamah <62065669+zsamamah@users.noreply.github.com>
2020-10-22 14:43:01 +0300

    Create app.py

---
+++ app.py
@@ -0,0 +1,64 @@
+from flask import Flask, render_template, request, redirect, url_for, jsonify, abort
+from flask_sqlalchemy import SQLAlchemy
+from flask_migrate import Migrate
+import sys
+
+app = Flask(__name__)
+app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://postgres:postgres@localhost:5432/todoapp'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS']=True
+
+db = SQLAlchemy(app)
+migrate = Migrate(app, db)
+
+class todo(db.Model):
+    __tablename__='todos'
+    id = db.Column(db.Integer , primary_key=True)
+    description = db.Column(db.String() , nullable =False)
+    completed = db.Column(db.Boolean, nullable=False, default=False)
+    
+    def __repr__(self):
+        return f'<Todo {self.id} {self.description}>'
+    
+#db.create_all()
+    
+@app.route('/todos/create',methods=['POST'])
+def crete_todo():
+  error = False
+  body = {}
+  try:
+    description = request.get_json()['description']
+    t = todo(description=description, completed=False)
+    db.session.add(t)
+    db.session.commit()
+    body['description'] = t.description
+    body['completed'] = t.completed
+  except:
+    error = True
+    db.session.rollback()
+    print(sys.exc_info)
+  finally:
+    db.session.close()
+  if error:
+        abort(400)
+  else:
+        return jsonify(body)
+  
+    
+
+@app.route('/todos/<todo_id>/set-completed', methods=['POST'])
+def set_completed_todo(todo_id):
+  try:
+    completed = request.get_json()['completed']
+    print('completed', completed)
+    t = todo.query.get(todo_id)
+    t.completed = completed
+    db.session.commit()
+  except:
+    db.session.rollback()
+  finally:
+    db.session.close()
+  return redirect(url_for('index.html'))
+    
+@app.route('/')
+def index():
+    return render_template('index.html', data=todo.query.all())
